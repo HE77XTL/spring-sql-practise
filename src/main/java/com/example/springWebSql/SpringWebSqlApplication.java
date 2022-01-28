32f703a66538346542992dce102446dd4d1b7bae
何东升 <he77505357@163.com>
2022-01-28 17:47:34 +0800

    spring-sql-practise

---
+++ src/main/java/com/example/springWebSql/SpringWebSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springWebSql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springWebSql/SpringWebSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springWebSql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
